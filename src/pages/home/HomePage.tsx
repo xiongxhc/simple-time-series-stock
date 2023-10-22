f3730fa99709e55161c153b380b38654c7bd0519
Chris Xiong <xionghc713@gmail.com>
2023-10-23 02:01:22 +0800

    able to use priceType

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,6 +3,7 @@ import './HomePage.css';
 import {Stack, Typography} from '@mui/material';
 import {TimeSeriesChart} from '../../components/time-series-chart/TimeSeriesChart';
 import StocksQuery from '../../query/stocks/StocksQuery';
+import {PriceType} from '../../api/finnhub-api/stocks/StockConst';
 
 function HomePage() {
   const stocksQuery = new StocksQuery();
@@ -12,7 +13,7 @@ function HomePage() {
       <header className="HomePage-header">
         <Typography variant="h3">Time Series Chart</Typography>
         <Stack>{JSON.stringify(symbols)}</Stack>
-        <TimeSeriesChart symbol={'AAPL'} timeframe={'D'} from={1691971459} to={1697971459} />
+        <TimeSeriesChart symbol={'AAPL'} timeframe={'D'} from={1691971459} to={1697971459} priceType={PriceType.CLOSE}/>
       </header>
     </div>
   );
