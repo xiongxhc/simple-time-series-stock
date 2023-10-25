65517b82942cc973a6a97413a65de7dac5a539cb
Chris Xiong <xionghc713@gmail.com>
2023-10-25 21:05:16 +0800

    remove css file

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
-import './HomePage.css';
-import {Typography} from '@mui/material';
+import React, {useState} from 'react';
+import dayjs, {Dayjs} from 'dayjs';
+import {Stack, Typography} from '@mui/material';
 import {TimeSeriesChart} from '../../components/time-series-chart/TimeSeriesChart';
 import StocksQuery from '../../query/stocks/StocksQuery';
 import {PriceType} from '../../api/finnhub-api/stocks/StockConst';
@@ -9,45 +9,58 @@ import {StockSelectionTable} from '../../components/stock-selection-table/StockS
 function HomePage() {
   const stocksQuery = new StocksQuery();
   const stockList = stocksQuery.getStockList();
-  const [rowSelectionModel, setRowSelectionModel] =
-    React.useState<string[]>([]);
+  const [rowSelectionModel, setRowSelectionModel] = React.useState<string[]>([]);
+  // const [startTime, setStartTime] = useState()
+  const [endTime, setEndTime] = useState<Dayjs | null>(dayjs('2022-04-17'));
 
-  useEffect(() => {
-    console.log(rowSelectionModel);
-  }, [rowSelectionModel]);
   return (
-    <div className="HomePage">
+    <Stack>
       <Typography
         sx={{backgroundColor: 'lightgray', position: 'absolute', margin: 2, padding: 2, borderRadius: 5}}
         variant="h3"
       >
         Time Series Chart
       </Typography>
-      <div className="HomePage-header">
-        <StockSelectionTable stockList={stockList ?? []} setRowSelectionModel={setRowSelectionModel} />
-        <TimeSeriesChart
-          symbol={rowSelectionModel[0]}
-          timeframe={'D'}
-          from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
-          to={new Date().getTime()}
-          priceType={PriceType.CLOSE}
-        />
-        <TimeSeriesChart
-          symbol={rowSelectionModel[1]}
-          timeframe={'D'}
-          from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
-          to={new Date().getTime()}
-          priceType={PriceType.CLOSE}
-        />
-        <TimeSeriesChart
-          symbol={rowSelectionModel[2]}
-          timeframe={'D'}
-          from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
-          to={new Date().getTime()}
-          priceType={PriceType.CLOSE}
-        />
-      </div>
-    </div>
+      <>
+        <Stack
+          sx={{
+            minHeight: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            color: 'white',
+          }}
+        >
+          <StockSelectionTable
+            stockList={stockList ?? []}
+            setRowSelectionModel={setRowSelectionModel}
+            endTime={endTime}
+            setEndTime={setEndTime}
+          />
+          <TimeSeriesChart
+            symbol={rowSelectionModel[0]}
+            timeframe={'D'}
+            from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
+            to={new Date().getTime()}
+            priceType={PriceType.CLOSE}
+          />
+          <TimeSeriesChart
+            symbol={rowSelectionModel[1]}
+            timeframe={'D'}
+            from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
+            to={new Date().getTime()}
+            priceType={PriceType.CLOSE}
+          />
+          <TimeSeriesChart
+            symbol={rowSelectionModel[2]}
+            timeframe={'D'}
+            from={new Date('2023-01-01T00:00:00.000Z').getTime() / 1000}
+            to={new Date().getTime()}
+            priceType={PriceType.CLOSE}
+          />
+        </Stack>
+      </>
+    </Stack>
   );
 }
 
